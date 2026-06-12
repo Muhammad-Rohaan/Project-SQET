dc7e83114eecef9b87f970a9dcb3bdb1c060e995
Muhammad-Rohaan <rohaanali82005@gmail.com>
2026-06-12 16:26:42 +0500

    Fixed Issues.

--- Pages/AdminPage.js
+++ Pages/AdminPage.js
@@ -1,8 +1,15 @@
+import LoginPage from "./LoginPage";
+import LoginData from "../testdata/LoginData.json";
+
+
+
 class AdminPage {
 
     constructor(page) {
         this.page = page;
+        this.navigateToDashboard = page.getByLabel('Navigate to Dashboard')
         this.addTeacherBtn = page.getByLabel('Add new Teacher');
+
         this.fullNameInput = page.getByLabel('Full Name');
         this.emailInput = page.getByLabel('Email Address');
         this.passwordInput = page.getByLabel('Password');
@@ -18,9 +25,51 @@ class AdminPage {
         this.ageInput = page.getByLabel('Age');
         this.registerBtn = page.getByRole('button', { name: 'Register Teacher' });
         this.cancelBtn = page.getByRole('button', { name: 'Cancel' });
+        // recp:
+        this.addReceptionistBtn = page.getByLabel('Add new Receptionist'); // Assuming naming convention matches addTeacherBtn
+        this.receptionRegIdInput = page.getByLabel('Registration ID');
+        this.registerReceptionistBtn = page.getByRole('button', { name: 'Register Receptionist' });
+
+        this.fullNameInput = page.getByLabel('Full Name');
+        this.emailInput = page.getByLabel('Email Address');
+        this.passwordInput = page.getByLabel('Password');
+        this.cnicInput = page.getByLabel('CNIC Number');
+        this.salaryInput = page.getByLabel('Monthly Salary');
+        this.joiningDateInput = page.getByLabel('Joining Date');
+        this.contactInput = page.getByLabel('Contact Number');
+        this.addressInput = page.getByLabel('Home Address');
+        // Announcement Section
+        this.postAnnouncementModalBtn = page.getByRole('button', {
+            name: '📢 Post Announcement'
+        })
+        this.announcementTitleInput = page.getByPlaceholder('Title');
+        this.announcementMessageInput = page.getByPlaceholder('Message');
+        this.announcementTargetDropdown = page.locator('select'); // Targeting the HTML select dropdown ('All')
+        this.postBtn = page.getByRole('button', {
+            name: 'Post',
+            exact: true
+        });
+
+        this.fullNameInput = page.getByLabel('Full Name');
+        this.emailInput = page.getByLabel('Email Address');
+        this.passwordInput = page.getByLabel('Password');
+        this.cnicInput = page.getByLabel('CNIC Number');
+        this.salaryInput = page.getByLabel('Monthly Salary');
+        this.joiningDateInput = page.getByLabel('Joining Date');
+        this.contactInput = page.getByLabel('Contact Number');
+        this.addressInput = page.getByLabel('Home Address');
+
     }
 
     async registerTeacher(teacherData) {
+        // Login first
+        const loginPage = new LoginPage(this.page);
+        const loginData = LoginData.validUsers[0]; // for admin because only admin can create Teacher
+        await loginPage.gotoUrl();
+        await loginPage.login(loginData.email, loginData.password, loginData.expectedMsg);
+
+        // Form Navigation & Interaction
+        await this.navigateToDashboard.click();
         await this.addTeacherBtn.click();
         await this.fullNameInput.fill(teacherData.fullName);
         await this.emailInput.fill(teacherData.email);
@@ -29,17 +78,52 @@ class AdminPage {
         await this.cnicInput.fill(teacherData.cnic);
         await this.qualificationInput.fill(teacherData.qualification);
         await this.salaryInput.fill(teacherData.salary);
-        await this.joiningDateInput.fill(teacherData.joiningDate);
+        await this.joiningDateInput.fill(teacherData.date);
+
         await this.subjectsInput.fill(teacherData.subjects);
         await this.classesInput.fill(teacherData.classes);
         await this.contactInput.fill(teacherData.contact);
         await this.addressInput.fill(teacherData.address);
         await this.ageInput.fill(teacherData.age);
         await this.registerBtn.click();
-
     }
 
+    async registerReceptionist(receptionistData) {
+        // Login first
+        const loginPage = new LoginPage(this.page);
+        const loginData = LoginData.validUsers[0]; // for admin because only admin can create Receptionist
+        await loginPage.gotoUrl();
+        await loginPage.login(loginData.email, loginData.password, loginData.expectedMsg);
+        // Form Navigation & Interaction
+        await this.navigateToDashboard.click();
+        await this.addReceptionistBtn.click();
+        await this.receptionRegIdInput.fill(receptionistData.receptionRegId);
+        await this.cnicInput.fill(receptionistData.cnic);
+        await this.salaryInput.fill(receptionistData.salary);
+        await this.joiningDateInput.fill(receptionistData.joiningDate);
+        await this.contactInput.fill(receptionistData.contact);
+        await this.addressInput.fill(receptionistData.address);
+        await this.registerReceptionistBtn.click();
+    }
 
+    async postAnnouncement(announcementData) {
+        // Login first
+        const loginPage = new LoginPage(this.page);
+        const loginData = LoginData.validUsers[0]; // for admin because only admin can create Receptionist
+        await loginPage.gotoUrl();
+        await loginPage.login(loginData.email, loginData.password, loginData.expectedMsg);
 
+        await this.postAnnouncementModalBtn.click();
+        await this.announcementTitleInput.fill(announcementData.title);
+        await this.announcementMessageInput.fill(announcementData.message);
+
+        await this.announcementTargetDropdown.selectOption({ value: announcementData.target });
+
+        if (announcementData.target === 'specific-class') {
+            await this.page.getByPlaceholder('Class').fill(announcementData.className);
+        }
+        await this.postBtn.click();
+    }
 
 }
+export default AdminPage;
--- tests/Admin.spec.js
+++ tests/Admin.spec.js
@@ -1,2 +1,27 @@
 import { test, expect } from "@playwright/test";
+import AdminPage from "../Pages/AdminPage";
+import AdminData from "../testdata/AdminData.json";
 
+test('TC#1 - Validating registration teachers | Positive', async ({page}) => {
+    const adminPage = new AdminPage(page);
+    const adminDataForTeacherRegistration = AdminData.validTeacher;
+
+    await adminPage.registerTeacher(adminDataForTeacherRegistration);
+    
+});
+
+test('TC#2 - Validating registration receptionist | Positive', async ({page}) => {
+    const adminPage = new AdminPage(page);
+    const adminDataForReceptionistRegistration = AdminData.validReceptionist;
+
+    await adminPage.registerReceptionist(adminDataForReceptionistRegistration);
+    
+});
+
+test('TC#3 - Validating posting announcement | Positive', async ({page}) => {
+    const adminPage = new AdminPage(page);
+    const adminDataForAnnouncement = AdminData.validAnnouncement;
+
+    await adminPage.postAnnouncement(adminDataForAnnouncement);
+    
+});
